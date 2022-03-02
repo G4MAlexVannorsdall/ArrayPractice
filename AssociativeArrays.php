e358f7cc1cd480d99212d8cfa99a7b614cae27cd
G4MAlexVannorsdall <94846507+G4MAlexVannorsdall@users.noreply.github.com>
2022-03-02 08:53:32 +0000

    first commit

---
+++ AssociativeArrays.php
@@ -0,0 +1,8 @@
+<?php
+// An array with a string index where each value is assigned a specific key.
+
+$People = ['William'=>'Taft', 'Howard'=>'Little', 'Betsy'=>'Cross', 'Loriane'=>'Alto', 'Dan'=>'Futura'];
+
+//echo $People['Betsy'] .PHP_EOL; //Finds the last name (value) of Betsy within the $People array.
+
+//echo $People['Cross'] . PHP_EOL; // This you can not do because Betsy is the key which is attached to the value.
---
+++ NumericArrays.php
@@ -0,0 +1,20 @@
+<?php
+//Accessing a specific index in the array
+
+$num_days_of_week = [1,2,3,4,5,6,7];
+
+//echo $num_days_of_week[2] . PHP_EOL; // Finds the 2nd index in the $num_days_of_week array which is 3.
+
+$months = [1,2,3,4,5,6,7,8,9,10,11,12];
+
+//echo $months[7] .PHP_EOL; // Finds the 7th index in the $months array which is 8.
+
+
+$People = ['William', 'Howard', 'John', 'Louis', 'Patrick'];
+
+//echo $People[4] . PHP_EOL; // Finds the 4th index in the $People array which is Patrick.
+
+$book_genres = ['fantasy', 'drama', 'historical', 'scifi', 'mystery', 'biography', 'dystopian'];
+
+//echo $book_genres[5] .PHP_EOL;// Finds the 5th index in the $book_genres array which is biography.
+
